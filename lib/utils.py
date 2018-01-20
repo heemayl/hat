93476a6f98597eaa2bd6ef6505952df0a7e8813a
heemayl <heemayl@gmail.com>
2018-01-20 18:22:14 +0600

    Added context manager for flock()

---
+++ lib/utils.py
@@ -0,0 +1,26 @@
+'''Miscellaneous utilities.'''
+
+import fcntl
+import os
+import time
+
+
+class FLock:
+    '''A context manager for exclusive locking (LOCK_EX) of files.'''
+    def __init__(self, lockfile_suffix=''):
+        self.lockfile = os.path.join(os.path.dirname(__file__),
+                                     '.lockfile_{}'.format(lockfile_suffix))
+
+    def __enter__(self):
+        self.lockf = open(self.lockfile, 'w')
+        while True:
+            try:
+                fcntl.lockf(self.lockf, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except BlockingIOError:
+                time.sleep(0.2)
+                continue
+            return self.lockf
+        
+    def __exit__(self, *exc_info):
+        fcntl.lockf(self.lockf, fcntl.LOCK_UN)
+        self.lockf.close()
